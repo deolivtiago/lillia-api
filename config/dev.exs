b0eb084700fefcc8f2b740044794c8d2b3b69e7c
Tiago de Oliveira <deolivtiago@outlook.com>
2025-08-12 19:58:14 -0300

    chore: set database config

--- config/dev.exs
+++ config/dev.exs
@@ -2,13 +2,13 @@ import Config
 
 # Configure your database
 config :lillia, LilliaCore.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
+  username: System.get_env("DB_USER", "postgres"),
+  password: System.get_env("DB_PASSWORD", "postgres"),
+  hostname: System.get_env("DB_HOST", "localhost"),
   database: "lillia_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: System.schedulers_online() * 2
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
