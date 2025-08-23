fb5fb69ca3b1ab1f87b6ecadffb3c06a067199fb
Tiago de Oliveira <deolivtiago@outlook.com>
2025-08-23 12:02:54 -0300

    feat: add context to send emails

---
+++ lib/lillia_core/access/emails.ex
@@ -0,0 +1,21 @@
+defmodule LilliaCore.Access.Emails do
+  @moduledoc """
+  `Access.Emails` context
+  """
+
+  alias LilliaCore.Access.Emails.VerificationCode
+
+  @doc ~S"""
+  Sends an `VerificationCode` to the user email
+
+  ## Examples
+
+      iex> send_verification_code(user, code)
+      {:ok, %Swoosh.Email{}}
+
+      iex> send_verification_code(user, code)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  defdelegate send_verification_code(user, code), to: VerificationCode.Send, as: :call
+end
