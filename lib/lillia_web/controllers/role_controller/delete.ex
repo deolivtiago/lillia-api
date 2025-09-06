0a0db1d34bd61fe24a934da2a0b51814ab0ba658
Tiago de Oliveira <deolivtiago@outlook.com>
2025-09-06 13:28:18 -0300

    feat: add controller to handle roles

---
+++ lib/lillia_web/controllers/role_controller/delete.ex
@@ -0,0 +1,31 @@
+defmodule LilliaWeb.RoleController.Delete do
+  @moduledoc false
+
+  import Ecto.Changeset
+
+  alias LilliaCore.Access.Roles
+
+  def handle(params) do
+    params
+    |> validate_params()
+    |> get_role()
+    |> delete_role()
+  end
+
+  defp validate_params(params) do
+    types = %{id: :string}
+
+    cast({%{}, types}, params, Map.keys(types))
+    |> validate_required(Map.keys(types))
+    |> validate_format(:id, ~r/^[a-zA-Z0-9_]+$/)
+    |> validate_length(:id, max: 64)
+    |> validate_exclusion(:id, ~w(root), message: "is invalid")
+    |> apply_action(:validate)
+  end
+
+  defp get_role({:ok, %{id: id}}), do: Roles.get_role(:id, id)
+  defp get_role({:error, changeset}), do: {:error, changeset}
+
+  defp delete_role({:ok, role}), do: Roles.delete_role(role)
+  defp delete_role({:error, changeset}), do: {:error, changeset}
+end
