ea22abc32ad2d5c5eaaf683ecedfbc26d24fc13d
Tiago de Oliveira <deolivtiago@outlook.com>
2025-12-07 22:47:19 -0300

    chore: include code analyzer

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule LilliaCore.MixProject do
       {:gettext, "~> 1.0"},
       {:jason, "~> 1.4"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.8"}
+      {:bandit, "~> 1.8"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
@@ -65,8 +66,20 @@ defmodule LilliaCore.MixProject do
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      precommit: ["compile --warning-as-errors", "deps.unlock --unused", "format", "test"]
+      test: [
+        "ecto.drop --quiet",
+        "ecto.create --quiet",
+        "ecto.migrate --quiet",
+        "run priv/repo/seeds.exs --quiet",
+        "test"
+      ],
+      precommit: [
+        "compile --warning-as-errors",
+        "deps.unlock --check-unused",
+        "format --check-formatted",
+        "credo --strict",
+        "test"
+      ]
     ]
   end
 end
